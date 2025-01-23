fb5f14b742d3c0f4b15bc074f5362989578c9007
Pournima <ponna.mhaskar@gmail.com>
2025-01-23 17:31:10 -0500

    Ask user for first name

--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,9 @@
 # include <stdio.h>
 int main () {
     char name[50];
-    printf("Please enter your name: ");
+    printf("Please enter your first name: ");
     scanf("%s", name);
-    printf ("Hello,World!\n");
+    printf ("Hello, %s !\n", name);
     return 0;
 }
 
